6b4bb9e5301caa1eaabae1f7d29748e49cc53dda
georgehajnal <128971944+georgehajnal@users.noreply.github.com>
2025-05-04 22:51:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <windows.h>
-#define maxx 1024
+#include <cstring>
+#include <sstream>
+#define maxx 10000
 using namespace std;
 ifstream in("input");
 
-char tape[30000], str[maxx];
+char tape[30000], str[maxx], v[maxx];
 int ptr;
-void banner() {
+string cuv;
 
+void banner() {
     cout<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                a                        "<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                                         "<<endl;
-    cout<<"b                                         "<<endl;
+    cout<<"                  ,,          ,..."<<endl;
+    cout<<"                 *MM        .d' **"<<endl;
+    cout<<"                  MM        dM`   "<<endl;
+    cout<<"`7M'    ,A    `MF'MM,dMMb. mMMmm  "<<endl;
+    cout<<"  VA   ,VAA   ,V  MM    `Mb MM    "<<endl;
+    cout<<"   VA ,V  VA ,V   MM     M8 MM    "<<endl;
+    cout<<"    VVV    VVV    MM.   ,M9 MM    "<<endl;
+    cout<<"     W      W     P^YbmdP'.JMML.  "<<endl;
     cout<<endl;
+    cout<<"Version 1.0"<<endl;
+    cout<<endl;
+
+
 }
 void bf(char input[maxx])
 {
@@ -85,19 +91,100 @@ int main()
     in.get(str, maxx);
     banner();
     cout<<"1. normal bf"<<endl;
-    cout<<"2. "<<endl;
-    cout<<"3. help"<<endl;
+    cout<<"2. word bf"<<endl;
+    cout<<"3. wbf to bf"<<endl;
+    cout<<"4. help"<<endl;
+    cout<<"5. quit"<<endl;
     cout<<endl;
     cout<<"Optiune: ";
     int ccc;
     cin>>ccc;
     system("cls");
 
-    if (ccc==3) {cout<<"pune programul in input si foloseste 1 sau 2"<<endl; return 0;}
+    if (ccc==4) {cout<<"pune programul in input si foloseste 1 sau 2"<<endl; return 0;}
+    if (ccc==5) {return 0;}
+    if (ccc==1) {
+        bf(str);
+        cout<<endl;
+    }
+    if (ccc==2) {
+        int h = -1;
+        istringstream iss(str);
+        char word[10];
+        while(iss >> word) {
+            int n = strlen(word);
+            int sum = 0;
+            for (int q=0;q<n;q++) {
+                sum+=word[q];
+            }
+            sum = sum % 8;
+            switch(sum) {
+                case 0:
+                v[++h]='>';
+                break;
+                case 1:
+                v[++h]='<';
+                break;
+                case 2:
+                v[++h]='+';
+                break;
+                case 3:
+                v[++h]='-';
+                break;
+                case 4:
+                v[++h]='.';
+                break;
+                case 5:
+                v[++h]=',';
+                break;
+                case 6:
+                v[++h]='[';
+                break;
+                case 7:
+                v[++h]=']';
+                break;
+            }
+        }
+        bf(v);
 
-    /*
-	bf(str);
-	cout<<endl;
-    */
+    }
+    if (ccc==3) {
+        istringstream iss(str);
+        char word[10];
+        while(iss >> word) {
+            int n = strlen(word);
+            int sum = 0;
+            for (int q=0;q<n;q++) {
+                sum+=word[q];
+            }
+            sum = sum % 8;
+            switch(sum) {
+                case 0:
+                cout<<'>';
+                break;
+                case 1:
+                cout<<'<';
+                break;
+                case 2:
+                cout<<'+';
+                break;
+                case 3:
+                cout<<'-';
+                break;
+                case 4:
+                cout<<'.';
+                break;
+                case 5:
+                cout<<',';
+                break;
+                case 6:
+                cout<<'[';
+                break;
+                case 7:
+                cout<<']';
+                break;
+            }
+        }
+    }
 	return 0;
 }
